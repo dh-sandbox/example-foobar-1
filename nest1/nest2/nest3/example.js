bb7d9c4adf3d6baef67dec4b4704effbf22cb25d
Dave Henton <dave@codeclimate.com>
2025-01-21 18:12:19 -0600

    Update example.js

--- nest1/nest2/nest3/example.js
+++ nest1/nest2/nest3/example.js
@@ -181,4 +181,929 @@ function isPrime(n){
   = true
 > isPrime(237);
   = false
+
+
+function isPrime(n){
+  var divisor = 2;
+roger
+roger
+roger
+roger
+rogerroger
+roger
+roger
+roger
+fixme
+FIXME
+FIXME
+FIXME
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+  while (n > divisor){
+    if(n % divisor == 0){
+     return false; 
+    }
+    else
+      divisor++;
+  }
+  return true;
+}
+
+> isPrime(137);
+  = true
+> isPrime(237);
+  = false
         
+        
+function isPrime(n){
+  var divisor = 2;
+
+  while (n > divisor){
+    if(n % divisor == 0){
+     return false; 
+    }
+    else
+      divisor++;
+  }
+  return true;
+}
+
+> isPrime(137);
+  = true
+> isPrime(237);
+  = false
+        
+        
+function isPrime(n){
+  var divisor = 2;
+
+  while (n > divisor){
+    if(n % divisor == 0){
+     return false; 
+    }
+    else
+      divisor++;
+  }
+  return true;
+}
+
+> isPrime(137);
+  = true
+> isPrime(237);
+  = false
+        
+        
+function isPrime(n){
+  var divisor = 2;
+
+  while (n > divisor){
+    if(n % divisor == 0){
+     return false; 
+    }
+    else
+      divisor++;
+  }
+  return true;
+}
+
+> isPrime(137);
+  = true
+> isPrime(237);
+  = false
+        
+        
+function isPrime(n){
+  var divisor = 2;
+
+  while (n > divisor){
+    if(n % divisor == 0){
+     return false; 
+    }
+    else
+      divisor++;
+  }
+  return true;
+}
+
+> isPrime(137);
+  = true
+> isPrime(237);
+  = false
+        
+
+
+function isPrime(n){
+  var divisor = 2;
+roger
+roger
+roger
+roger
+rogerroger
+roger
+roger
+roger
+fixme
+FIXME
+FIXME
+FIXME
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+  while (n > divisor){
+    if(n % divisor == 0){
+     return false; 
+    }
+    else
+      divisor++;
+  }
+  return true;
+}
+
+> isPrime(137);
+  = true
+> isPrime(237);
+  = false
+        
+        
+function isPrime(n){
+  var divisor = 2;
+
+  while (n > divisor){
+    if(n % divisor == 0){
+     return false; 
+    }
+    else
+      divisor++;
+  }
+  return true;
+}
+
+> isPrime(137);
+  = true
+> isPrime(237);
+  = false
+        
+        
+function isPrime(n){
+  var divisor = 2;
+
+  while (n > divisor){
+    if(n % divisor == 0){
+     return false; 
+    }
+    else
+      divisor++;
+  }
+  return true;
+}
+
+> isPrime(137);
+  = true
+> isPrime(237);
+  = false
+        
+        
+function isPrime(n){
+  var divisor = 2;
+
+  while (n > divisor){
+    if(n % divisor == 0){
+     return false; 
+    }
+    else
+      divisor++;
+  }
+  return true;
+}
+
+> isPrime(137);
+  = true
+> isPrime(237);
+  = false
+        
+        
+function isPrime(n){
+  var divisor = 2;
+
+  while (n > divisor){
+    if(n % divisor == 0){
+     return false; 
+    }
+    else
+      divisor++;
+  }
+  return true;
+}
+
+> isPrime(137);
+  = true
+> isPrime(237);
+  = false
+        
+
+
+function isPrime(n){
+  var divisor = 2;
+roger
+roger
+roger
+roger
+rogerroger
+roger
+roger
+roger
+fixme
+FIXME
+FIXME
+FIXME
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+  while (n > divisor){
+    if(n % divisor == 0){
+     return false; 
+    }
+    else
+      divisor++;
+  }
+  return true;
+}
+
+> isPrime(137);
+  = true
+> isPrime(237);
+  = false
+        
+        
+function isPrime(n){
+  var divisor = 2;
+
+  while (n > divisor){
+    if(n % divisor == 0){
+     return false; 
+    }
+    else
+      divisor++;
+  }
+  return true;
+}
+
+> isPrime(137);
+  = true
+> isPrime(237);
+  = false
+        
+        
+function isPrime(n){
+  var divisor = 2;
+
+  while (n > divisor){
+    if(n % divisor == 0){
+     return false; 
+    }
+    else
+      divisor++;
+  }
+  return true;
+}
+
+> isPrime(137);
+  = true
+> isPrime(237);
+  = false
+        
+        
+function isPrime(n){
+  var divisor = 2;
+
+  while (n > divisor){
+    if(n % divisor == 0){
+     return false; 
+    }
+    else
+      divisor++;
+  }
+  return true;
+}
+
+> isPrime(137);
+  = true
+> isPrime(237);
+  = false
+        
+        
+function isPrime(n){
+  var divisor = 2;
+
+  while (n > divisor){
+    if(n % divisor == 0){
+     return false; 
+    }
+    else
+      divisor++;
+  }
+  return true;
+}
+
+> isPrime(137);
+  = true
+> isPrime(237);
+  = false
+        
+
+
+function isPrime(n){
+  var divisor = 2;
+roger
+roger
+roger
+roger
+rogerroger
+roger
+roger
+roger
+fixme
+FIXME
+FIXME
+FIXME
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+  while (n > divisor){
+    if(n % divisor == 0){
+     return false; 
+    }
+    else
+      divisor++;
+  }
+  return true;
+}
+
+> isPrime(137);
+  = true
+> isPrime(237);
+  = false
+        
+        
+function isPrime(n){
+  var divisor = 2;
+
+  while (n > divisor){
+    if(n % divisor == 0){
+     return false; 
+    }
+    else
+      divisor++;
+  }
+  return true;
+}
+
+> isPrime(137);
+  = true
+> isPrime(237);
+  = false
+        
+        
+function isPrime(n){
+  var divisor = 2;
+
+  while (n > divisor){
+    if(n % divisor == 0){
+     return false; 
+    }
+    else
+      divisor++;
+  }
+  return true;
+}
+
+> isPrime(137);
+  = true
+> isPrime(237);
+  = false
+        
+        
+function isPrime(n){
+  var divisor = 2;
+
+  while (n > divisor){
+    if(n % divisor == 0){
+     return false; 
+    }
+    else
+      divisor++;
+  }
+  return true;
+}
+
+> isPrime(137);
+  = true
+> isPrime(237);
+  = false
+        
+        
+function isPrime(n){
+  var divisor = 2;
+
+  while (n > divisor){
+    if(n % divisor == 0){
+     return false; 
+    }
+    else
+      divisor++;
+  }
+  return true;
+}
+
+> isPrime(137);
+  = true
+> isPrime(237);
+  = false
+        
+
+
+function isPrime(n){
+  var divisor = 2;
+roger
+roger
+roger
+roger
+rogerroger
+roger
+roger
+roger
+fixme
+FIXME
+FIXME
+FIXME
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+  while (n > divisor){
+    if(n % divisor == 0){
+     return false; 
+    }
+    else
+      divisor++;
+  }
+  return true;
+}
+
+> isPrime(137);
+  = true
+> isPrime(237);
+  = false
+        
+        
+function isPrime(n){
+  var divisor = 2;
+
+  while (n > divisor){
+    if(n % divisor == 0){
+     return false; 
+    }
+    else
+      divisor++;
+  }
+  return true;
+}
+
+> isPrime(137);
+  = true
+> isPrime(237);
+  = false
+        
+        
+function isPrime(n){
+  var divisor = 2;
+
+  while (n > divisor){
+    if(n % divisor == 0){
+     return false; 
+    }
+    else
+      divisor++;
+  }
+  return true;
+}
+
+> isPrime(137);
+  = true
+> isPrime(237);
+  = false
+        
+        
+function isPrime(n){
+  var divisor = 2;
+
+  while (n > divisor){
+    if(n % divisor == 0){
+     return false; 
+    }
+    else
+      divisor++;
+  }
+  return true;
+}
+
+> isPrime(137);
+  = true
+> isPrime(237);
+  = false
+        
+        
+function isPrime(n){
+  var divisor = 2;
+
+  while (n > divisor){
+    if(n % divisor == 0){
+     return false; 
+    }
+    else
+      divisor++;
+  }
+  return true;
+}
+
+> isPrime(137);
+  = true
+> isPrime(237);
+  = false
+        
+
