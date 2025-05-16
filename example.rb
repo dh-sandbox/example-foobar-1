63797797c5d81a4472a336769e4e6dabf1067d72
Dave Henton <dave@codeclimate.com>
2025-05-16 11:46:16 -0500

    update

--- example.rb
+++ example.rb
@@ -5,3 +5,26 @@ class Example
 end
 
 
+class Example
+  def answer
+    42
+  end
+end
+
+
+
+class Example
+  def answer
+    42
+  end
+end
+
+
+
+class Example
+  def answer
+    42
+  end
+end
+
+
