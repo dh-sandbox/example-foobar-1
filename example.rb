40838bb3d11b5e60971ef596d4ec535459611e7d
Dave Henton <dave@codeclimate.com>
2025-05-16 12:10:23 -0500

    Update example.rb
    only `send qlty check status` is enabled

--- example.rb
+++ example.rb
@@ -11,13 +11,49 @@ class Example
   end
 end
 
-
+asdfasdfa
 
 class Example
   def answer
     42
   end
 end
+
+class Example
+  def answer
+    42
+  end
+end
+
+asdfasdfa
+
+class Example
+  def answer
+    42
+    
+class Example
+  def answer
+    42
+  end
+end
+
+asdfasdfa
+
+class Example
+  def answer
+    42
+    
+class Example
+  def answer
+    42
+  end
+end
+
+asdfasdfa
+
+class Example
+  def answer
+    42
 
 
 
