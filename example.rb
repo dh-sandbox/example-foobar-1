954392d2c2200394f43544ae29480906a1fade97
Dave Henton <dave@codeclimate.com>
2025-05-16 11:29:22 -0500

    Update example.rb
    dummy commit after `fail on any new issues" is enabled

--- example.rb
+++ example.rb
@@ -53,3 +53,113 @@ class Example
   class Example
   def answer
     42
+class Example
+  def answer
+    42
+  end
+end
+
+
+class Example
+asdfadf
+  end
+end
+
+
+
+class Example
+  def answer
+    42
+  end
+end
+
+
+
+asdf
+
+asdf
+class Example
+  def answer
+    42
+  end
+
+  class Example
+  def answer
+    42
+  end
+
+    
+asdf
+class Example
+  def answer
+    42
+  end
+
+  class Example
+  def answer
+    42
+    
+asdf
+class Example
+  def answer
+    42
+  end
+
+  class Example
+  def answer
+    42
+class Example
+  def answer
+    42
+  end
+end
+
+
+class Example
+asdfadf
+  end
+end
+
+
+
+class Example
+  def answer
+    42
+  end
+end
+
+
+
+asdf
+
+asdf
+class Example
+  def answer
+    42
+  end
+
+  class Example
+  def answer
+    42
+  end
+
+    
+asdf
+class Example
+  def answer
+    42
+  end
+
+  class Example
+  def answer
+    42
+    
+asdf
+class Example
+  def answer
+    42
+  end
+
+  class Example
+  def answer
+    42
