0be150c6ccb9e3ad5e8d2e6ee7db4922d7a4715c
Dave Henton <dave@codeclimate.com>
2025-05-16 12:12:02 -0500

    Update example.rb

--- example.rb
+++ example.rb
@@ -1,4 +1,257 @@
+
 class Example
+  def answer
+    42
+  end
+end
+
+
+class Example
+  def answer
+    42
+  end
+end
+
+asdfasdfa
+
+class Example
+  def answer
+    42
+  end
+end
+
+class Example
+  def answer
+    42
+  end
+end
+
+asdfasdfa
+
+class Example
+  def answer
+    42
+    
+class Example
+  def answer
+    42
+  end
+end
+
+asdfasdfa
+
+class Example
+  def answer
+    42
+    
+class Example
+  def answer
+    42
+  end
+end
+
+asdfasdfa
+
+class Example
+  def answer
+    42
+
+
+
+class Example
+  def answer
+    42
+  end
+end
+
+
+class Example
+  def answer
+    42
+  end
+end
+
+
+class Example
+  def answer
+    42
+  end
+end
+
+
+
+class Example
+  def answer
+    42
+  end
+end
+
+
+
+class Example
+  def answer
+    42
+  end
+end
+
+
+class Example
+  def answer
+    42
+  end
+end
+
+
+class Example
+  def answer
+    42
+  end
+end
+
+
+
+class Example
+  def answer
+    42
+  end
+end
+
+
+
+class Example
+  def answer
+    42
+  end
+end
+
+
+    class Example
+  def answer
+    42
+  end
+end
+
+
+class Example
+  def answer
+    42
+  end
+end
+
+asdfasdfa
+
+class Example
+  def answer
+    42
+  end
+end
+
+class Example
+  def answer
+    42
+  end
+end
+
+asdfasdfa
+
+class Example
+  def answer
+    42
+    
+class Example
+  def answer
+    42
+  end
+end
+
+asdfasdfa
+
+class Example
+  def answer
+    42
+    
+class Example
+  def answer
+    42
+  end
+end
+
+asdfasdfa
+
+class Example
+  def answer
+    42
+
+
+
+class Example
+  def answer
+    42
+  end
+end
+
+
+class Example
+  def answer
+    42
+  end
+end
+
+
+class Example
+  def answer
+    42
+  end
+end
+
+
+
+class Example
+  def answer
+    42
+  end
+end
+
+
+
+class Example
+  def answer
+    42
+  end
+end
+
+
+class Example
+  def answer
+    42
+  end
+end
+
+
+class Example
+  def answer
+    42
+  end
+end
+
+
+
+class Example
+  def answer
+    42
+  end
+end
+
+
+
+class Example
+  def answer
+    42
+  end
+end
+
+
+    class Example
   def answer
     42
   end
