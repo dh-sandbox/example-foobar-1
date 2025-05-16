2da484cb4daeb0b61cb35bf731db7b532512450b
Dave Henton <dave@codeclimate.com>
2025-05-16 11:41:27 -0500

    Update example.rb
    commit after Workspace settings were applied to all projects, with only "send qlty status" enabled

--- example.rb
+++ example.rb
@@ -36,9 +36,72 @@ class Example
     42
   end
 end
+  end
+endasdfasdfclass Example
+asdfadf
+  end
+endasdfasdfclass Example
+asdfadf
+  end
+endasdfasdfclass Example
+asdfadf
+  end  end
+endasdfasdfclass Example
+asdfadf
+  end
+endasdfasdfclass Example
+asdfadf
+  end
+endasdfasdfclass Example  end
+endasdfasdfclass Example
+asdfadf
+  end
+endasdfasdfclass Example
+asdfadf
+  end
+endasdfasdfclass Example
+asdfadf
+  end
+endasdfasdfclass Example
+asdfadf
+  end
+endasdfasdfclass Example
+asdfadf
+  end
+endasdfasdf
+class Example
+  def answer
+    42
+asdfadf
+  end
+endasdfasdfclass Example
+asdfadf
+  end
+endasdfasdfclass Example
+asdfadf
+  end
+endasdfasdf
+class Example
+  def answer
+    42endasdfasdfclass Example
+asdfadf
+  end
+endasdfasdfclass Example
+asdfadf
+  end
+endasdfasdf
+class Example
+  def answer
+    42duasdfduasdf
+duasdf
 
+duasdf
+duasdf
+duasdf
 
+duasdf
 
+duasdf
 asdf
 
 asdf
